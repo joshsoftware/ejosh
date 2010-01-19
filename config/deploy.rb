ad247528fc166574f37ef10ed82db1cff8124b08
Shailesh Patil <pshailesh1612@gmail.com>
2010-01-19 17:42:45 +0530

    changed the deployment script to git

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,16 @@
 set :application, "eJosh"
-set :repository,  "svn://shailesh@server.joshsoftware.com/eJosh/trunk/src/eJosh"
+set :repository,  "git://github.com/joshsoftware/ejosh.git"
 
-set :scm, :subversion
-set :scm_username,       "shailesh"
-set :scm_password,       "shailesh"
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "intranet.joshsoftware.com" # Your HTTP server, Apache/etc
-role :app, "intranet.joshsoftware.com", :mongrel => true, :sphinx => true, :memcached => true
+role :web, "joshsoftware.com" # Your HTTP server, Apache/etc
+role :app, "joshsoftware.com", :mongrel => true, :sphinx => true, :memcached => true
 #role :app, "192.168.1.100:10000"   # This may be the same as your `Web` server
-role :db,  "intranet.joshsoftware.com", :primary => true # This is where Rails migrations will run
+role :db,  "joshsoftware.com", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
-set :deploy_to, "/home/shailesh/eJosh"
+set :deploy_to, "/home/shailesh/eJosh_latest"
 set :use_sudo, false
 
 set :rails_env, "production"
