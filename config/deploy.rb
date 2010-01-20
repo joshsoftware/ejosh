99b5fbd89e2168481cddce507b098af11d145525
Shailesh Patil <pshailesh1612@gmail.com>
2010-01-20 13:58:31 +0530

    added a link of rails_tiny_mce submodule in the .gitmodule file

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,16 +4,16 @@ set :repository,  "git://github.com/joshsoftware/ejosh.git"
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "joshsoftware.com" # Your HTTP server, Apache/etc
-role :app, "joshsoftware.com", :mongrel => true, :sphinx => true, :memcached => true
+role :web, "192.168.1.100" # Your HTTP server, Apache/etc
+role :app, "192.168.1.100", :mongrel => true, :sphinx => true, :memcached => true
 #role :app, "192.168.1.100:10000"   # This may be the same as your `Web` server
-role :db,  "joshsoftware.com", :primary => true # This is where Rails migrations will run
+role :db,  "192.168.1.100", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
 
 set :deploy_to, "/home/shailesh/eJosh_latest"
 set :use_sudo, false
-
 set :rails_env, "production"
+set :git_enable_submodules, 1
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
